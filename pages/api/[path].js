650d3c2e0d6b45e20ab996fb6bad9f596510ca8e
spuf <spufru@gmail.com>
2021-12-04 18:51:20 +0300

    poll

--- pages/api/[path].js
+++ pages/api/[path].js
@@ -11,17 +11,29 @@ const bot = new Telegraf(token, {
   telegram: { webhookReply: true },
 })
 
-bot.telegram.setWebhook(base + path)
+const keyboard = Markup.keyboard([Markup.button.pollRequest('Play time poll', 'regular')])
 
-bot.start((ctx) => ctx.reply(`I don't even have time to explain why I don't have time to explain.`))
+bot.start((ctx) => ctx.reply(`I don't even have time to explain why I don't have time to explain.`), keyboard)
+
+bot.command('poll', (ctx) =>
+  ctx.replyWithPoll('When you are ready to play?', ['In 1 hour', '2 hours', '3 hours', '4 hours', '5 hours'], { is_anonymous: false })
+)
 
 export default function handler(req, res) {
-  if (req.query.path !== path) {
-    return res.status(404).end()
-  }
-  if (req.method !== 'POST') {
-    return res.status(405).end()
+  if (path === 'init') {
+    if (req.method !== 'GET') {
+      return res.status(405).end()
+    }
+
+    return bot.telegram.setWebhook(base + path).then(() => res.status(200).end())
   }
 
-  bot.handleUpdate(req.body).finally(() => res.status(200).end())
+  if (req.query.path === path) {
+    if (req.method !== 'POST') {
+      return res.status(405).end()
+    }
+    return bot.handleUpdate(req.body).finally(() => res.status(200).end())
+  }
+
+  return res.status(404).end()
 }
