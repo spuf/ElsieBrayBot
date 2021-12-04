b5721d6b46254c74f43bfc3aa7f86d9ae7511b3c
spuf <556061+spuf@users.noreply.github.com>
2021-12-04 18:13:55 +0300

    secure weboohok

--- pages/api/[path].js
+++ pages/api/[path].js
@@ -4,15 +4,23 @@ const token = process.env.BOT_TOKEN
 if (token === undefined) {
   throw new Error('BOT_TOKEN must be provided!')
 }
+const path = (process.env.BOT_HOOK_PATH ?? '/') + token
 
 const bot = new Telegraf(token, {
   telegram: { webhookReply: true },
 })
 
-bot.telegram.setWebhook(process.env.BOT_HOOK_PATH ?? '/')
+bot.telegram.setWebhook(path)
 
 bot.start((ctx) => ctx.reply(`I don't even have time to explain why I don't have time to explain.`))
 
 export default function handler(req, res) {
+  if (req.query.path !== token) {
+    return res.status(404).end()
+  }
+  if (req.method !== 'POST') {
+    return res.status(405).end()
+  }
+
   bot.handleUpdate(req.body).finally(() => res.status(200).end())
 }
