c71ef0232615d8a5dd8f38ad5da2a4e0701d5f1e
spuf <556061+spuf@users.noreply.github.com>
2021-12-06 18:25:46 +0300

    login demo

--- pages/user.js
+++ pages/user.js
@@ -5,7 +5,7 @@ export default function User() {
   if (session) {
     return (
       <div>
-        Signed in as {session.user.email} <br />
+        Signed in as {session.user.name} <br />
         <button onClick={() => signOut()}>Sign out</button>
       </div>
     )
