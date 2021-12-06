19f58568207c50c7926ab3210c8f104a7e8664f8
spuf <spufru@gmail.com>
2021-12-06 18:10:52 +0300

    auth demo

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,12 +2,13 @@ import NextAuth from 'next-auth'
 import BungieProvider from 'next-auth/providers/bungie'
 
 export default NextAuth({
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     BungieProvider({
         clientId: process.env.BUNGIE_CLIENT_ID,
         clientSecret: process.env.BUNGIE_SECRET,
         headers: {
-          'X-API-Key': provess.env.BUNGIE_API_KEY
+          'X-API-Key': process.env.BUNGIE_API_KEY
         }
       }),
   ],
