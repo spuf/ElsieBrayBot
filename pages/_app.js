977a148654a65d8cea3491bbc4a4d9c4329b8b9e
spuf <spufru@gmail.com>
2021-12-07 17:11:56 +0300

    layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 function ElsieBray({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="container">
+      <Head>
+        <title>ElsieBrayBot</title>
+      </Head>
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </div>
+  )
 }
 
 export default ElsieBray
