ac8f3335b03731e2b1cf5f62f356895ab1c9d5fb
spuf <556061+spuf@users.noreply.github.com>
2021-12-08 14:40:37 +0300

    login full

--- pages/guardian.js
+++ pages/guardian.js
@@ -1,17 +1,56 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 
-export default function Guardian({ message, url }) {
+export default function Guardian({ message, url, token }) {
   const router = useRouter()
+  const [data, setData] = useState({
+    token: null,
+  })
   useEffect(() => {
+    if (token) {
+      localStorage.setItem('token', token)
+    }
     if (url) {
       setTimeout(() => {
         router.replace(url)
       }, 1000)
+    } else {
+      const token = localStorage.getItem('token')
+      if (token) {
+        fetch(`/api/auth/check?token=${token}`)
+          .then((res) => res.json())
+          .then((data) => setData({ token, ...data }))
+          .catch((e) => {
+            console.error(e)
+            localStorage.removeItem('token')
+            router.reload()
+          })
+      }
     }
-  })
+  }, [url, router, token, setData])
 
+  if (!data.token) {
+    return (
+      <div className="center">
+        <p>
+          <Link href="/">
+            <a>← Return to Orbit</a>
+          </Link>
+        </p>
+        <p>{message}</p>
+        {url && (
+          <p>
+            <Link href={url} replace>
+              <a>Continue →</a>
+            </Link>
+          </p>
+        )}
+      </div>
+    )
+  }
+  console.log(data)
+  const { telegram_username, bungie_username } = data
   return (
     <div className="center">
       <p>
@@ -19,35 +58,28 @@ export default function Guardian({ message, url }) {
           <a>← Return to Orbit</a>
         </Link>
       </p>
-      <p>{message}</p>
-      {url && (
-        <p>
-          <Link href={url} replace>
-            <a>Continue →</a>
-          </Link>
-        </p>
-      )}
+      <p>
+        @{telegram_username} is {bungie_username}
+      </p>
     </div>
   )
 }
 
 export async function getServerSideProps({ query, resolvedUrl }) {
   const url = new URL(resolvedUrl, process.env.BASE_URL)
-  let data
-
+  let props = null
   if (query.id && query.hash) {
     url.pathname = '/api/auth/telegram'
     const res = await fetch(url.toString())
-    data = await res.json()
+    props = await res.json()
   } else if (query.state) {
     url.pathname = '/api/auth/bungie'
     const res = await fetch(url.toString())
-    data = await res.json()
-  } else {
-    data = { message: 'You must start login from Telegram.' }
+    props = await res.json()
+  }
+  if (!props) {
+    props = { message: 'You must start login from Telegram.' }
   }
 
-  return {
-    props: data,
-  }
+  return { props }
 }
