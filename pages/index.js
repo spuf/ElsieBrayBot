79e316847dbcbf84911580a221b4b6d0116ad7d0
spuf <556061+spuf@users.noreply.github.com>
2021-12-09 13:16:51 +0300

    link

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,26 @@
 import avatar from '../public/avatar.jpg'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Index() {
   const size = 122
   return (
     <div className="center">
-      <div
-        style={{
-          width: `${size}px`,
-          height: `${size}px`,
-          borderRadius: `${size / 2}px`,
-          margin: '1em auto',
-          overflow: 'hidden',
-        }}
-      >
-        <Image src={avatar} alt="Avatar" placeholder="blur" />
-      </div>
+      <Link href="/guardian">
+        <a>
+          <div
+            style={{
+              width: `${size}px`,
+              height: `${size}px`,
+              borderRadius: `${size / 2}px`,
+              margin: '1em auto',
+              overflow: 'hidden',
+            }}
+          >
+            <Image src={avatar} alt="Avatar" placeholder="blur" />
+          </div>
+        </a>
+      </Link>
 
       <h1>ElsieBrayBot</h1>
 
