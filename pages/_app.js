c82e3ba39de04932b4f9f6ccd09c22c337c95c81
spuf <spufru@gmail.com>
2021-12-09 16:58:16 +0300

    progress bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import '../styles/globals.css'
 import Head from 'next/head'
+import NextNProgress from 'nextjs-progressbar'
 
 function ElsieBray({ Component, pageProps }) {
   return (
     <div className="container">
+      <NextNProgress />
       <Head>
         <title>ElsieBrayBot</title>
       </Head>
