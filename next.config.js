07c723ec474f7a6dace417f3a1dd2b703146397e
spuf <spufru@gmail.com>
2021-12-11 16:11:04 +0300

    sentry

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 // @ts-check
 
 const { withSentryConfig } = require('@sentry/nextjs')
-const Sentry = require('@sentry/nextjs')
-
-Sentry.init({
-  dsn: process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN,
-  tracesSampleRate: 1.0,
-})
 
 /**
  * @type {import('next').NextConfig}
