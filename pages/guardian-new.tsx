bbe66b1f66375656077b6761dec099403d13d889
spuf <556061+spuf@users.noreply.github.com>
2021-12-25 13:19:09 +0300

    new page

---
+++ pages/guardian-new.tsx
@@ -0,0 +1,61 @@
+import type { GetServerSideProps, InferGetServerSidePropsType } from 'next'
+import Image from 'next/image'
+import Link from 'next/link'
+import styles from '../styles/Guardian.module.css'
+import type { AuthResponse } from './api/auth'
+
+function Character({ character }) {
+  return (
+    <div className={styles.emblem}>
+      <Image className={styles.background} src={character.emblemPath} alt="Emblem" layout="fill" objectFit="contain" />
+      <span className={styles.light}>{character.light}</span>
+    </div>
+  )
+}
+
+export default function Guardian({ user }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  return (
+    <div className="center">
+      <p>
+        <Link href="/">
+          <a>← Return to Orbit</a>
+        </Link>
+      </p>
+      <p>
+        Telegram:{' '}
+        <a href={`https://t.me/${user.telegram_username}`} target="_blank" rel="noreferrer">
+          @{user.telegram_username}
+        </a>
+      </p>
+      <p>
+        Bungie.net:{' '}
+        <a href="https://www.bungie.net/7/ru/User/Profile/254/100002/bungie" target="_blank" rel="noreferrer">
+          {user.bungie.uniqueName}
+        </a>
+      </p>
+      <Character character={user.character} />
+    </div>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps<AuthResponse> = async (ctx) => {
+  const props = {
+    user: {
+      telegram_id: '100001',
+      telegram_username: 'tgname',
+      bungie: {
+        membershipId: '100002',
+        uniqueName: 'bungie#3006',
+      },
+      character: {
+        characterId: '100003',
+        membershipId: '100004',
+        membershipType: 100,
+        light: 1440,
+        emblemPath: 'https://www.bungie.net/common/destiny2_content/icons/30304f81bf77ad95f5176aa706e20a96.jpg',
+      },
+    },
+  }
+
+  return { props }
+}
