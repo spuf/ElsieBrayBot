2eeaf2d1413e7b86c5b93f1304f6fc96de109323
spuf <spufru@gmail.com>
2021-12-25 13:53:27 +0300

    sec headers

--- next.config.js
+++ next.config.js
@@ -6,9 +6,39 @@ const { withSentryConfig } = require('@sentry/nextjs')
  * @type {import('next').NextConfig}
  **/
 const nextConfig = {
+  poweredByHeader: false,
   reactStrictMode: true,
   images: {
     domains: ['www.bungie.net'],
+  },
+  headers: async function () {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin',
+          },
+        ],
+      },
+    ]
   },
 }
 
